b8d25e2008e69b7a0d399c05c06b97d77bf2b0a7
Stanico <ashadknowles@gmail.com>
2023-02-18 10:48:20 -0500

    add chapter awards modules

---
+++ src/api/chapter-award/controllers/chapter-award.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chapter-award controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::chapter-award.chapter-award');
---
+++ src/api/chapter-award/routes/chapter-award.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chapter-award router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chapter-award.chapter-award');
