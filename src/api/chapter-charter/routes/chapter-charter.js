fe8fc5d0925346a64e42043000c187f20ba9ed25
Stanico <ashadknowles@gmail.com>
2023-02-18 10:48:46 -0500

    add chapter charters

---
+++ src/api/chapter-charter/routes/chapter-charter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chapter-charter router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chapter-charter.chapter-charter');
---
+++ src/api/chapter-charter/services/chapter-charter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chapter-charter service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::chapter-charter.chapter-charter');
