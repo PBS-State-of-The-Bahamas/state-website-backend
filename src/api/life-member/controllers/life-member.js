15494f2e6f0ce0d617113306438c8e1dffb442a9
Tanielle Arthur <taniellecurtis@Tanielles-MacBook-Pro.local>
2023-02-28 21:51:58 -0500

    add life member to strapi

---
+++ src/api/life-member/controllers/life-member.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * life-member controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::life-member.life-member');
