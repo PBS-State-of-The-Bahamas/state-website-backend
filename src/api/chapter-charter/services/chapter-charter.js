c51e0091e6f16a172a51f0965ad289fb1a2e699b
Stanico <ashadknowles@gmail.com>
2023-03-03 15:50:03 -0500

    remove chapter charters

--- src/api/chapter-charter/services/chapter-charter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * chapter-charter service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::chapter-charter.chapter-charter');
