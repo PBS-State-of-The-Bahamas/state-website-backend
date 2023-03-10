57a8458c1812dac34df7c5a5d221d215eb1f1284
quincyarthur <quincya@nib-bahamas.com>
2023-03-10 17:17:29 -0500

    add env variable for customizable admin url

--- config/admin.js
+++ config/admin.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   url: env(
     "ADMIN_URL",
-    `http://${env("HOST", "localhost")}:${env.int("PORT", 1337)}/admin`
+    `http://${env("HOST", "0.0.0.0")}:${env.int("PORT", 1337)}/admin`
   ),
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
