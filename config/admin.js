177d05f4b963cad483d885affc2cf168ac5d53c9
quincyarthur <quincya@nib-bahamas.com>
2023-03-10 13:10:11 -0500

    add env to customize atrapi admin url

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,12 @@
 module.exports = ({ env }) => ({
+  url: env(
+    "ADMIN_URL",
+    `http://${env("HOST", "localhost")}:${env.int("PORT", 1337)}/admin`
+  ),
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
 });
