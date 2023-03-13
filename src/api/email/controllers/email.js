bf81215e080f743d37c9bcc28bdb870b49550513
Stanico <ashadknowles@gmail.com>
2023-03-12 20:45:28 -0400

    add validation for email request

--- src/api/email/controllers/email.js
+++ src/api/email/controllers/email.js
@@ -2,6 +2,17 @@ const templateIDs = {
   CHAPTER_INTEREST: process.env.SENDGRID_CHAPTER_INTEREST_TEMPLATE_ID,
 };
 
+const validators = {
+  CHAPTER_INTEREST: (options) => {
+    const errors = [];
+    if (!options.data.fullName) errors.push("Full name is required");
+    if (!options.data.email) errors.push("Email is required");
+    if (!options.data.phone) errors.push("Phone is required");
+    if (!options.chapter) errors.push("Chapter is required");
+    return errors;
+  },
+};
+
 const EmailTypes = {
   CHAPTER_INTEREST: async (options) => {
     const chapter = await strapi.entityService.findMany(
@@ -26,7 +37,17 @@ const EmailTypes = {
 
 module.exports = {
   send: async (ctx) => {
+    let errors;
     const options = ctx.request.body;
+    if (Object.keys(validators).includes(options.form)) {
+      errors = validators[options.form](options);
+    } else {
+      return ctx.response.badRequest("Submission invalid. Cannot validate.");
+    }
+    if (errors.length)
+      return ctx.response.badRequest("Your submission has errors", {
+        errors: errors,
+      });
     try {
       const email = await EmailTypes[options.form](options);
       await strapi.plugin("email").service("email").send({
