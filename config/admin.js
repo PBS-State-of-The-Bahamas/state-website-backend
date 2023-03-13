e8a52d5651cbadd97887760e237179dccd614d15
quincyarthur <quincya@nib-bahamas.com>
2023-03-13 13:38:02 -0400

    default to original config

--- config/admin.js
+++ config/admin.js
@@ -1,11 +1,4 @@
 module.exports = ({ env }) => ({
-  url: env(
-    "ADMIN_URL",
-    `http://${env("STRAPI_HOST", "0.0.0.0")}:${env.int(
-      "STRAPI_PORT",
-      1337
-    )}/admin`
-  ),
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
   },
