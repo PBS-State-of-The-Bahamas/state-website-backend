4d7c59f4d1ea42eea1066d5f3fcdedd871a637e5
Lion-el-Smith <lionelsmith_1997@hotmail.com>
2023-03-13 20:09:37 -0400

    added strapi backend components for state history

--- src/api/state-history/routes/state-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * state-history router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::state-history.state-history');
--- src/api/state-history/services/state-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * state-history service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::state-history.state-history');
