60d537fb23bbdf1cc3901dcff70845b1c6e0700e
Stanico Knowles <65465649+Stanico-Knowles@users.noreply.github.com>
2023-03-28 18:38:18 +0000

    add content for state home page

---
+++ src/api/state-home-page/routes/state-home-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * state-home-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::state-home-page.state-home-page');
